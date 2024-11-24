500257d9fda758a47c9a34563440d1b3016d20e5
Developer Barry <barryallen2558@gmail.com>
2024-11-24 22:47:18 +0600

    add left side slider

--- art-and-craft-store/src/layouts/HeroSection/HeroSection.jsx
+++ art-and-craft-store/src/layouts/HeroSection/HeroSection.jsx
@@ -7,6 +7,15 @@ import "swiper/css/effect-fade";
 const CustomCarousel = () => {
   return (
     <div className="flex justify-between items-center gap-8">
+      {/* Left static image */}
+      <div className=" h-[300px] flex justify-center items-center">
+        <img
+          src="https://i.ibb.co.com/gZGs1xH/slider1.png"
+          alt="Left"
+          className="object-cover w-full h-full"
+        />
+      </div>
+
 
     </div>
   );
