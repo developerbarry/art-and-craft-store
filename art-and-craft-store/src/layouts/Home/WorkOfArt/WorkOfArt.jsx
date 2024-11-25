0f916eae1d51fd0942343488e5e1b91f8e5820df
Developer Barry <barryallen2558@gmail.com>
2024-11-25 19:54:37 +0600

    added animation on work art sec

--- art-and-craft-store/src/layouts/Home/WorkOfArt/WorkOfArt.jsx
+++ art-and-craft-store/src/layouts/Home/WorkOfArt/WorkOfArt.jsx
@@ -9,57 +9,57 @@ const WorkOfArt = () => {
                 <div>
                     <div className="grid grid-cols-4 gap-5 py-5 px-14 max-w-screen-xl mx-auto grid-flow-dense">
 
-                        <img className="col-span-2 row-span-2 w-full h-full object-cover rounded-md"
+                        <img className="col-span-2 row-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/zS81npc/12.jpg"
                             alt="" />
 
                         <img
-                            className="w-full h-full object-cover rounded-md"
+                            className="w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/sVHkH1t/11.jpg"
                             alt="" />
 
                         <img
-                            className="w-full h-full object-cover rounded-md"
+                            className="w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/VTVY1Jh/10.jpg"
                             alt="" />
 
                         <img
-                            className="row-span-2 w-full h-full object-cover rounded-md"
+                            className="row-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/bmmrd2V/05.jpg"
                             alt="" />
 
                         <img
-                            className="row-span-2 w-full h-full object-cover rounded-md"
+                            className="row-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/JBSfgyP/09.jpg"
                             alt="" />
 
                         <img
-                            className="w-full h-full object-cover rounded-md"
+                            className="w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/4W2hchN/08.jpg"
                             alt="" />
 
                         <img
-                            className="row-span-2 w-full h-full object-cover rounded-md"
+                            className="row-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/QH0sVNs/07.jpg"
                             alt="" />
 
                         <img
-                            className="col-span-2 w-full h-full object-cover rounded-md"
+                            className="col-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/C9KzHHf/04.jpg"
                             alt="" />
 
                         <img
-                            className="row-span-2 w-full h-full object-cover rounded-md"
+                            className="row-span-2 w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/ZBsRTGf/06.jpg"
                             alt="" />
 
                         <img
-                            className="w-full h-full object-cover rounded-md"
+                            className="w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/YdG4wtD/02.jpg"
                             alt="" />
 
                         <img
-                            className="w-full h-full object-cover rounded-md"
+                            className="w-full h-full object-cover rounded-md hover:scale-105 transition-transform duration-300 ease-in-out cursor-zoom-in"
                             src="https://i.ibb.co/HNmYt7d/03.jpg"
                             alt="" />
                     </div>
