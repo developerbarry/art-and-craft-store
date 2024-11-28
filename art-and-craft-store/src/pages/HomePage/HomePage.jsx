a70fc1fe89dd8006f6c68d16ab3f8c2fbab65ac6
Developer Barry <barryallen2558@gmail.com>
2024-11-28 19:03:38 +0600

    add skeleton loader in homepage also

--- art-and-craft-store/src/pages/HomePage/HomePage.jsx
+++ art-and-craft-store/src/pages/HomePage/HomePage.jsx
@@ -8,12 +8,19 @@ import { useEffect, useState } from 'react';
 const HomePage = () => {
 
     const [carftItems, setCarftItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const skeletonItems = new Array(6).fill(null);
 
-    useEffect(()=> {
+
+    useEffect(() => {
+        setIsLoading(true)
         fetch('http://localhost:5000/six-craft-items')
-        .then(res => res.json())
-        .then(data => setCarftItems(data))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setCarftItems(data)
+                setIsLoading(false)
+            })
+    }, [])
 
     console.log(carftItems)
 
@@ -23,7 +30,7 @@ const HomePage = () => {
                 <HeroSection />
             </section>
             <section className='py-16'>
-                <WorkOfArt/>
+                <WorkOfArt />
             </section>
             <section>
                 <DescriptionCard />
@@ -33,9 +40,27 @@ const HomePage = () => {
                     <div>
 
                     </div>
+
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-10 px-4 md:px-16'>
                         {
-                            carftItems.map(carftItem => <CarftItemCard carftItem={carftItem}  key={carftItem._id}/>)
+                            isLoading ? (
+                                skeletonItems.map((ignored, index) => (
+                                    <div key={index} className="flex flex-col rounded shadow-md w-full md:max-w-[350px] animate-pulse h-96">
+                                        <div className="h-56 rounded-t dark:bg-gray-300"></div>
+                                        <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
+                                            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
+                                            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
+                                            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
+                                        </div>
+                                    </div>
+                                ))
+
+                            ) :
+
+                                (
+                                    carftItems.map(carftItem => <CarftItemCard carftItem={carftItem} key={carftItem._id} />)
+                                )
+
                         }
                     </div>
                 </div>
