15d962cbdcc589e7c4f14d8a07376e9cbfb3680b
Developer Barry <barryallen2558@gmail.com>
2024-11-29 03:22:03 +0600

    delete method apply in client side

--- art-and-craft-store/src/componets/CaftAndArtCard/CarftAndArtCard.jsx
+++ art-and-craft-store/src/componets/CaftAndArtCard/CarftAndArtCard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const CarftAndArtCard = ({ artAndCraftItem, myOwnItem }) => {
@@ -11,6 +12,35 @@ const CarftAndArtCard = ({ artAndCraftItem, myOwnItem }) => {
         item_name,
         subcategory_Name,
         price, rating } = artAndCraftItem;
+
+    const handleDeleteItem = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/all-art-and-craft-items/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+
+
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+            }
+        });
+    }
 
 
     return (
@@ -48,12 +78,12 @@ const CarftAndArtCard = ({ artAndCraftItem, myOwnItem }) => {
 
 
 
-            
+
             {
                 myOwnItem && (
                     <div className="absolute top-0 flex justify-between w-full ">
                         <Link to={`/all-art-and-craft-items/my-art-and-craft/${_id}/edit`} className="bg-teal-200 p-2 rounded-full hover:scale-110 transition"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg></Link>
-                        <button className="bg-teal-200 p-2 rounded-full hover:scale-110 transition"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>
+                        <button onClick={handleDeleteItem} className="bg-teal-200 p-2 rounded-full hover:scale-110 transition"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>
                     </div>
                 )
             }
