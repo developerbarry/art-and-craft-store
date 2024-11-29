242759169605de3e3e54ac10152290b240020280
Developer Barry <barryallen2558@gmail.com>
2024-11-29 13:51:24 +0600

    subcate data load and display in UI

--- art-and-craft-store/src/componets/SubCategoryItems/SubCategoryItems.jsx
+++ art-and-craft-store/src/componets/SubCategoryItems/SubCategoryItems.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import CarftAndArtCard from "../CaftAndArtCard/CarftAndArtCard";
 
 const SubCategoryItems = () => {
-
+    const [subcategoryDatas, setSubcategoryDatas] = useState([])
     const { subcategory } = useParams();
     console.log(subcategory)
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/all-art-and-craft-items/subcategory/${subcategory}`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setSubcategoryDatas(data)
+            })
+    }, [subcategory])
+
+    console.log(subcategoryDatas)
+
     return (
-        <div>
-            <h1>This is Sub Category Cards</h1>
-        </div>
+        <section>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-6 py-14 px-4">
+                    {
+                        subcategoryDatas.map((subcategoryData) => <CarftAndArtCard
+                            key={subcategoryData._id}
+                            artAndCraftItem={subcategoryData}
+                        />)
+                    }
+                </div>
+            </div>
+        </section>
     );
 };
 
