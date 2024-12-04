48b25fbb6ca4619ee83540b6151d37ce661e1222
Developer Barry <barryallen2558@gmail.com>
2024-12-05 03:45:12 +0600

    Added new changes

---
+++ art-and-craft-store-server/index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USRE}:${process.env.DB_PASS}@cluster0.iam7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("CarftAndArtDB");
+        const allArtAndCraft = database.collection("artAndCraft")
+        const sixCraftItems = database.collection("mainSixCategories");
+
+
+        app.get('/six-craft-items', async (req, res) => {
+            const query = sixCraftItems.find();
+            const result = await query.toArray();
+            res.send(result)
+        })
+
+        app.get('/all-art-and-craft-items', async (req, res) => {
+            let query = {};
+
+            const cursor = allArtAndCraft.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+
+        app.get('/all-art-and-craft-items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await allArtAndCraft.findOne(query);
+            res.send(result)
+        })
+
+
+        app.post('/all-art-and-craft-items', async (req, res) => {
+            const item = req.body;
+            const result = await allArtAndCraft.insertOne(item);
+            res.send(result)
+        })
+
+        app.put('/all-art-and-craft-items/:id', async (req, res) => {
+            const id = req.params.id;
+            const item = req.body;
+            console.log(id, item)
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    image: item?.image,
+                    item_name: item?.item_name,
+                    subcategory_Name: item?.subcategory_Name,
+                    short_description: item?.short_description,
+                    price: item?.price,
+                    rating: item?.rating,
+                    customization: item?.customization,
+                    processing_time: item?.processing_time,
+                    stockStatus: item?.stockStatus
+                }
+            }
+
+            const result = await allArtAndCraft.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+
+        app.delete('/all-art-and-craft-items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await allArtAndCraft.deleteOne(query);
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+    res.send("Carft and Art DB")
+})
+
+app.listen(port, () => {
+    console.log(`Open this Port by ${port}`)
+})
---
+++ art-and-craft-store/src/pages/AllArtAndCraftList/AllArtAndCraftList.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import CarftAndArtCard from "../../componets/CaftAndArtCard/CarftAndArtCard";
+
+const AllArtAndCraftList = () => {
+
+    const [artAndCraftItems, setArtAndCraftItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const skeletonItems = new Array(8).fill(null);
+
+
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('http://localhost:5000/all-art-and-craft-items')
+            .then(res => res.json())
+            .then(data => {
+
+                setArtAndCraftItems(data);
+                setIsLoading(false);
+            })
+    }, [])
+
+
+
+    return (
+        <section>
+            <div className="container mx-auto">
+                <div className="py-14">
+                    <div className="text-center px-10 md:px-20 lg:px-40">
+                        <h1 className="text-[40px] font-yan text-[#3e454c]">Explore the World of Art & Craft</h1>
+                        <p className="font-raj text-base font-normal mb-5">Unleash your creativity and discover a curated collection of exquisite art and craft items. From breathtaking paintings to intricate handmade creations, our selection showcases the beauty of artistic expression and craftsmanship. Whether you&apos;re looking for inspiration, decoration, or a unique gift, you&apos;ll find pieces that bring imagination to life and add a touch of elegance to any space.</p>
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-6 py-14 px-4">
+
+                        {
+                            isLoading ? (
+
+                                skeletonItems.map((_, index) => (
+                                    <div key={index} className="flex flex-col rounded shadow-md w-full animate-pulse h-96">
+                                        <div className="h-48 rounded-t dark:bg-gray-300"></div>
+                                        <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
+                                            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
+                                            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
+                                            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
+                                        </div>
+                                    </div>
+                                ))
+
+                            ) :
+
+                                (
+                                    artAndCraftItems.map((artAndCraftItem) => <CarftAndArtCard key={artAndCraftItem._id} artAndCraftItem={artAndCraftItem} />)
+                                )
+
+                        }
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AllArtAndCraftList;
+
+
+// min-h-[calc(100vh-100px)]
